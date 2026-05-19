bbd09132226d892cd1c21cbc5ae0f97828f0fc5e
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2026-05-19 17:20:32 +0200

    chore(tests): add NOLINT comments for include statements in constexpr_tests.cpp and tests.cpp to enhance code cleanliness

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,3 +1,5 @@
-#include <catch2/catch_test_macros.hpp>  // NOLINT(misc-include-cleaner)
+// NOLINTBEGIN(*-include-cleaner)
+#include <catch2/catch_test_macros.hpp>  
 
-#include <Vantablade/vantablade.hpp>  // NOLINT(misc-include-cleaner)
+#include <Vantablade/vantablade.hpp>
+// NOLINTEND(*-include-cleaner)
