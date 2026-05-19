f32f3c1ac73d37b97e8b7111e366aa6a74cf4ac6
Clang Robot <robot@example.com>
2026-05-19 15:21:03 +0000

    :art: Applied clang-format to source code (auto-formatting) - Updated .h, .cpp, and .hpp files to conform with the style guide. - Changes include:   - Indentation adjustments   - Spacing improvements   - Alignment enhancements   - Consistency in formatting across files   - Removal of trailing whitespace

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,5 @@
 // NOLINTBEGIN(*-include-cleaner)
-#include <catch2/catch_test_macros.hpp>  
+#include <catch2/catch_test_macros.hpp>
 
 #include <Vantablade/vantablade.hpp>
 // NOLINTEND(*-include-cleaner)
