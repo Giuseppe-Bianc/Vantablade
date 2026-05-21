8e7580f832937d19106d16306128a7a23cf1b2f6
Clang Robot <robot@example.com>
2026-05-21 22:30:28 +0000

    :art: Applied clang-format to source code (auto-formatting) - Updated .h, .cpp, and .hpp files to conform with the style guide. - Changes include:   - Indentation adjustments   - Spacing improvements   - Alignment enhancements   - Consistency in formatting across files   - Removal of trailing whitespace

--- src/vantablade_lib/GameObject.cpp
+++ src/vantablade_lib/GameObject.cpp
@@ -1,6 +1,6 @@
 /*
-* Created by gbian on 22/05/2026.
-* Copyright (c) 2026 All rights reserved.
-*/
+ * Created by gbian on 22/05/2026.
+ * Copyright (c) 2026 All rights reserved.
+ */
 
 #include "GameObject.hpp"
