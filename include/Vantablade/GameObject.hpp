ea5a0c6573077e15a4f00b0125d94ac3c10e0f47
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2026-05-22 10:56:40 +0200

    fix(transform): add const qualifier to mat2() method and improve formatting
    - Changed the mat2() method in Transform2dComponent to be a const method, ensuring it does not modify the object state.
    - Improved code formatting by replacing '.0f' with '0.0f' for consistency in scaleMat initialization.

--- include/Vantablade/GameObject.hpp
+++ include/Vantablade/GameObject.hpp
@@ -13,12 +13,11 @@ struct Transform2dComponent {
     glm::vec2 scale{1.f, 1.f};
     float rotation;
 
-    glm::mat2 mat2() {
+    [[nodiscard]] glm::mat2 mat2() const {
         const float s = glm::sin(rotation);
         const float c = glm::cos(rotation);
-        glm::mat2 rotMatrix{{c, s}, {-s, c}};
-
-        glm::mat2 scaleMat{{scale.x, .0f}, {.0f, scale.y}};
+        const glm::mat2 rotMatrix{{c, s}, {-s, c}};
+        const glm::mat2 scaleMat{{scale.x, 0.0f}, {0.0f, scale.y}};
         return rotMatrix * scaleMat;
     }
 };
