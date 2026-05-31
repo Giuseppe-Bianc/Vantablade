ec07280c14cc9b0df1ae59327ce83c167e2d3349
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2026-05-31 20:54:17 +0200

    feat(KeyboardMovementController): implement movement and rotation controls for game objects
    - Added KeyboardMovementController class to handle keyboard input for movement and rotation.
    - Defined KeyMappings struct for customizable key bindings.
    - Implemented moveInPlaneXZ method to update game object position and rotation based on key presses.
    - Integrated movement in XZ plane and rotation limits for pitch values.
    - Updated CMakeLists.txt to include new source and header files for the KeyboardMovementController.

---
+++ include/Vantablade/KeyboardMovementController.hpp
@@ -0,0 +1,31 @@
+/*
+* Created by gbian on 31/05/2026.
+* Copyright (c) 2026 All rights reserved.
+*/
+
+#pragma once
+
+#include "Vantablade/Window.hpp"
+#include "Vantablade/GameObject.hpp"
+
+class KeyboardMovementController {
+public:
+    struct KeyMappings {
+        int moveLeft = GLFW_KEY_A;
+        int moveRight = GLFW_KEY_D;
+        int moveForward = GLFW_KEY_W;
+        int moveBackward = GLFW_KEY_S;
+        int moveUp = GLFW_KEY_E;
+        int moveDown = GLFW_KEY_Q;
+        int lookLeft = GLFW_KEY_LEFT;
+        int lookRight = GLFW_KEY_RIGHT;
+        int lookUp = GLFW_KEY_UP;
+        int lookDown = GLFW_KEY_DOWN;
+    };
+
+    void moveInPlaneXZ(GLFWwindow* window, float dt, GameObject& gameObject);
+
+    KeyMappings keys{};
+    float moveSpeed{3.f};
+    float lookSpeed{1.5f};
+};
---
+++ src/vantablade_lib/KeyboardMovementController.cpp
@@ -0,0 +1,35 @@
+/*
+* Created by gbian on 31/05/2026.
+* Copyright (c) 2026 All rights reserved.
+*/
+
+#include "Vantablade/KeyboardMovementController.hpp"
+
+void KeyboardMovementController::moveInPlaneXZ(GLFWwindow *window, float dt, GameObject &gameObject) {
+    glm::vec3 rotate{0};
+    if(glfwGetKey(window, keys.lookRight) == GLFW_PRESS) { rotate.y += 1.f; }
+    if(glfwGetKey(window, keys.lookLeft) == GLFW_PRESS) { rotate.y -= 1.f; }
+    if(glfwGetKey(window, keys.lookUp) == GLFW_PRESS) { rotate.x += 1.f; }
+    if(glfwGetKey(window, keys.lookDown) == GLFW_PRESS) { rotate.x -= 1.f; }
+
+    if(glm::length2(rotate) > fepsilon) { gameObject.transform.rotation += lookSpeed * dt * glm::normalize(rotate); }
+
+    // limit pitch values between about +/- 85ish degrees
+    gameObject.transform.rotation.x = glm::clamp(gameObject.transform.rotation.x, -1.5f, 1.5f);
+    gameObject.transform.rotation.y = glm::mod(gameObject.transform.rotation.y, glm::two_pi<float>());
+
+    float yaw = gameObject.transform.rotation.y;
+    const glm::vec3 forwardDir{C_F(std::sin(yaw)), 0.f, C_F(std::cos(yaw))};
+    const glm::vec3 rightDir{forwardDir.z, 0.f, -forwardDir.x};
+    const glm::vec3 upDir{0.f, -1.f, 0.f};
+
+    glm::vec3 moveDir{0.f};
+    if(glfwGetKey(window, keys.moveForward) == GLFW_PRESS) { moveDir += forwardDir; }
+    if(glfwGetKey(window, keys.moveBackward) == GLFW_PRESS) { moveDir -= forwardDir; }
+    if(glfwGetKey(window, keys.moveRight) == GLFW_PRESS) { moveDir += rightDir; }
+    if(glfwGetKey(window, keys.moveLeft) == GLFW_PRESS) { moveDir -= rightDir; }
+    if(glfwGetKey(window, keys.moveUp) == GLFW_PRESS) { moveDir += upDir; }
+    if(glfwGetKey(window, keys.moveDown) == GLFW_PRESS) { moveDir -= upDir; }
+
+    if(glm::length2(moveDir) > fepsilon) { gameObject.transform.translation += moveSpeed * dt * glm::normalize(moveDir); }
+}
