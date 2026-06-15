140b89c12922e36c3d46cb5b3e020eaec7369da9
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2026-06-15 19:14:23 +0200

    improvement(model):  use designated-initializers

--- src/vantablade_lib/Model.cpp
+++ src/vantablade_lib/Model.cpp
@@ -186,10 +186,12 @@ std::vector<VkVertexInputBindingDescription> Model::Vertex::getBindingDescriptio
 std::vector<VkVertexInputAttributeDescription> Model::Vertex::getAttributeDescriptions() {
     std::vector<VkVertexInputAttributeDescription> attributeDescriptions{};
 
-    attributeDescriptions.push_back({0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, position)});
-    attributeDescriptions.push_back({1, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, color)});
-    attributeDescriptions.push_back({2, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, normal)});
-    attributeDescriptions.push_back({3, 0, VK_FORMAT_R32G32_SFLOAT, offsetof(Vertex, uv)});
+    attributeDescriptions.push_back(
+        {.location = 0, .binding = 0, .format = VK_FORMAT_R32G32B32_SFLOAT, .offset = offsetof(Vertex, position)});
+    attributeDescriptions.push_back({.location = 1, .binding = 0, .format = VK_FORMAT_R32G32B32_SFLOAT, .offset = offsetof(Vertex, color)});
+    attributeDescriptions.push_back(
+        {.location = 2, .binding = 0, .format = VK_FORMAT_R32G32B32_SFLOAT, .offset = offsetof(Vertex, normal)});
+    attributeDescriptions.push_back({.location = 3, .binding = 0, .format = VK_FORMAT_R32G32_SFLOAT, .offset = offsetof(Vertex, uv)});
     return attributeDescriptions;
 }
 
